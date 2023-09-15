de8784e790f4076f28e77722f2fb2e8f1a3342b3
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-15 12:09:09 +0500

    script added

---
+++ v1/service_catalog.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var anchor = document.createElement('a');
+  var link = document.createTextNode('Service Catalog');
+  anchor.appendChild(link);
+  anchor.href = 'asdasd';
+  $('#user-nav').prepend(anchor);
+  alert('dasdas');
+});
