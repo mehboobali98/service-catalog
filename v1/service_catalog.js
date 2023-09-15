1781b0d7b4e8bf206b10fade22b35231271af974
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-15 15:34:17 +0500

    custom page url added

--- v1/service_catalog.js
+++ v1/service_catalog.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   var anchor = document.createElement('a');
   var link   = document.createTextNode('Service Catalog');
   anchor.appendChild(link);
-  anchor.href = 'asdasd';
+  anchor.href = '/hc/p/service_catalog';
   $('#user-nav').prepend(anchor);
 });
