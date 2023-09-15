3122931d13fb5216bb52d789c5afc14493c5d0c6
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-15 16:57:07 +0500

    refactor

--- v1/service_catalog.js
+++ v1/service_catalog.js
@@ -1,7 +1,18 @@
+import { addMenuItem, buildServiceCatalog } from './service_catalog_builder.js';
+
 $(document).ready(function(){
-  var anchor = document.createElement('a');
-  var link   = document.createTextNode('Service Catalog');
-  anchor.appendChild(link);
-  anchor.href = '/hc/p/service_catalog';
-  $('#user-nav').prepend(anchor);
+  addMenuItem('Service Catalog', '/hc/p/service_catalog', 'user-nav');
+  initServiceCatalog();
 });
+
+function initServiceCatalog() {
+  if (!isServiceCatalogPage()) { return; }
+
+  buildServiceCatalog();
+}
+
+function isServiceCatalogPage() {
+  const pattern  = /\/hc(\/en-us)?\/p\/service_catalog/;
+  const pathname = window.location.pathname;
+  return pattern.test(pathname);
+}
