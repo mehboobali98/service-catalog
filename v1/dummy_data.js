f4ba75532a474030fc3d83b8be6c3d5b65248d6f
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-15 16:56:09 +0500

    dummy data file added for service categories

---
+++ v1/dummy_data.js
@@ -0,0 +1,11 @@
+function serviceCatalogCategories() {
+  var categories = [
+                      { text: 'Home', link: '#' },
+                      { text: 'About', link: '#' },
+                      { text: 'Services', link: '#' },
+                      { text: 'Contact', link: '#' },
+                   ];
+  return categories;
+}
+
+export { serviceCatalogCategories }
