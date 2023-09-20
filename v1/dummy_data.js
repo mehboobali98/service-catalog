99a9877eec2681bc7316d4e1966d6297fbe720e7
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-20 17:22:20 +0500

    search bar added

--- v1/dummy_data.js
+++ v1/dummy_data.js
@@ -1,14 +1,75 @@
-function serviceCategories() {
-  return [
-    'My IT Assets',
-    'View Raised Requests',
-    'General IT Help',
-    'HR Services',
-    'Request New Software',
-    'Request Laptops',
-    'Request Mobile Devices',
-    'Software Access'
-  ].map(text => ({ text, link: '#' }));
+const serviceCategories = [
+  'My IT Assets',
+  'View Raised Requests',
+  'General IT Help',
+  'HR Services',
+  'Request New Software',
+  'Request Laptops',
+  'Request Mobile Devices',
+  'Software Access'
+];
+
+const serviceCatogoriesItems = [
+  'My IT Assets': {
+    description: 'List of everything within your custody. Easily report issues with your assigned devices or software.',
+    serviceItems: [
+      { 
+        name:    'Dell Insipiron 15 311',
+        img_src: 'adasd',
+        display_fields: [ 
+          { label: 'Serial #',    value: 'DSW42F4F' },
+          { label: 'Assigned On', value: '7 June 2021' }
+        ]
+      },
+      {
+        name:    'Jabra Evolve Headset',
+        img_src: 'asdasd',
+        display_fields: [
+          { label: 'Serial #',    value: 'DSW42F4F' },
+          { label: 'Assigned On', value: '7 June 2021' }
+        ]
+      },
+      {
+        name:    'Zoom',
+        img_src: 'asdasd',
+        display_fields: [
+          { label: 'Seats Given', value: '1' },
+          { label: 'Assigned On', value: '7 June 2021' }
+        ]
+      }
+    ]
+  },
+  'Request New Software': {
+    description: 'Browse and request available software for your needs or request a new one!',
+    serviceItems: [
+      {
+        name:    'Request New Software',
+        img_src: 'asdsad',
+        description: "Can't find the software you're looking for? Request a new one!"
+      },
+      {
+        name:
+        img_src: 'sdasd',
+        description: 'Slack description'
+      }
+    ]
+  }
+];
+
+function getServiceCategories() {
+  return serviceCategories.map(serviceCategory => ({ id: generateId(str), name: serviceCategory, link: '#' }));
 }
 
-export { serviceCategories };
+function getServiceCategoriesItems() {
+  return serviceCatogoriesItems;
+}
+
+function getServiceCategoryItems(serviceCategory) {
+  return serviceCatogoriesItems[serviceCategory];
+}
+
+function generateId(str) {
+  str.toLowerCase().replace(/\s+/g, "_");
+}
+
+export { getServiceCategories, getServiceCategoriesItems, getServiceCategoryItems };
--- v1/service_catalog_builder.js
+++ v1/service_catalog_builder.js
@@ -1,4 +1,4 @@
-import { serviceCategories } from './dummy_data.js';
+import { getServiceCategories, getServiceCategoriesItems, getServiceCategoryItems } from './dummy_data.js';
 
 function addMenuItem(name, url, parent_ele) {
   var anchor = document.createElement('a');
@@ -9,35 +9,72 @@ function addMenuItem(name, url, parent_ele) {
 }
 
 function buildServiceCatalog() {
-  const newSection = $('<section>');
-  newSection.attr('id', 'service-catalog-section');
+  const newSection = $('<section>').attr('id', 'service-catalog-section');
 
   const serviceCatalogContainer = $('<div>').addClass('row');
 
-  const navbarContainer = $('<div>').addClass('col-4');
+  const searchAndNavContainer = $('<div>').addClass('col-4');
+
+  const searchBar = $('<div>').append($('<input>').attr('id', 'search-input').attr('type', 'text').attr('placeholder', 'Search...'));
+
+  const navbarContainer = $('<div>');
   const navbar = generateNavbar(serviceCategories());
   navbarContainer.append(navbar);
 
-  const serviceItemsContainer = $('<div>').addClass('col-8');
+  searchAndNavContainer.append(searchBar, navbarContainer)
+
+  //const serviceItemsContainer = buildServiceCategoriesItems();
 
   // Append the navbar to the container
   serviceCatalogContainer.append(navbarContainer);
-  serviceCatalogContainer.append(serviceItemsContainer);
+  //serviceCatalogContainer.append(serviceItemsContainer);
 
   newSection.append(serviceCatalogContainer);
   $('main').append(newSection);
 }
 
 // Create a function to generate the vertical navbar
-function generateNavbar(navItems) {
-  var navbar = $('<ul></ul>');
+function generateNavbar(serviceCategories) {
+  const navbar = $('<ul></ul>');
 
-  $.each(navItems, function(index, item) {
-    var listItem = $('<li><a href="' + item.link + '">' + item.text + '</a></li>');
+  $.each(serviceCategories, function(index, serviceCategory) {
+    var listItem = $('<li><a id="' + serviceCategory.id + '_link''" + href="' + serviceCategory.link + '">' + serviceCategory.name + '</a></li>');
     navbar.append(listItem);
   });
 
   return navbar;
+}
+
+function buildServiceCategoriesItems() {
+  const serviceCategories     = Object.keys(getServiceCategoriesItems());
+  const serviceItemsContainer = $('<div>').addClass('col-8');
+  const serviceCategoryItems = getServiceCategoryItems(serviceCategory);
+
+  $.each(serviceCategories, function(index, serviceCategory) {
+    serviceItemsContainer.append(buildServiceCategoryItems(serviceCategory, serviceCategoryItems[serviceCategory]));
+  });
+
+  return serviceItemsContainer;
+}
+
+function buildServiceCategoryItems(serviceCategory, serviceCategoryItems) {
+  const serviceCategoryItemsContainer = $('<div>');
+  serviceCategoryItemsContainer.attr('id', serviceCategory.toLowerCase().replace(/\s+/g, "_"));
+
+  const serviceCategoryLabel = $('<p></p>').text(serviceCategory);
+  const serviceCategoryDescription = $('<p></p>').text(serviceCategoryItems.description);
+
+  serviceCategoryItemsContainer.append(serviceCategoryLabel).append(serviceCategoryDescription);
+
+  const serviceCategoryItemsFlex = $('<div>').addClass('d-flex');
+
+  $.each(serviceCategoryItems, function(index, serviceCategoryItem) {
+    serviceCategoryItemsFlex.append(buildServiceCategoryItem(serviceCategoryItem));
+  });
+}
+
+function buildServiceCategoryItem(serviceCategoryItem) {
+  k
 }
 
 export { addMenuItem, buildServiceCatalog };
