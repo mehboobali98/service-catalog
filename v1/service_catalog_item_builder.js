65f4477ebd62862ef5d9116b6b96933aa5c249fc
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-20 22:29:27 +0500

    item builder js added

---
+++ v1/service_catalog_item_builder.js
@@ -0,0 +1,46 @@
+function buildServiceCategoryItem(serviceCategoryItem) {
+  const serviceItemType = serviceCategoryItem.type;
+  switch (serviceItemType) {
+    case 'assigned_it_asset':
+      return buildItAssetServiceItem(serviceCategoryItem);
+    case 'assigned_software_entitlement':
+      return buildSoftwareServiceItem(serviceCategoryItem);
+    case 'software_request':
+      return buildSoftwareRequestServiceItem(serviceCategoryItem);
+    default:
+      // Handle unknown service type
+      return null;
+  }
+}
+
+function buildItAssetServiceItem(serviceCategoryItem) {
+  const card = $('<div>').addClass('row service-item-card');
+
+  // Create the card image element
+  const cardImageContainer = $('<div>').addClass('col-4');
+  const cardImage = $('<img>').attr('src', serviceCategoryItem.img_src)
+                              .attr('alt', 'IT Asset');
+  cardImageContainer.append(cardImage);
+
+  // Create the card body
+  const cardBody = $('<div>').addClass('col-8');
+
+  // Create the card title
+  const cardTitle = $('<h4>').text(serviceCategoryItem.name);
+  cardBody.append(cardTitle);
+
+  $.each(serviceCategoryItem.display_fields, function(index, field) {
+    var cardField = $('<p>').append($('<span>').text(field.label + ':' + field.value));
+    cardBody.append(cardField);
+  });
+
+  // submit request button
+  const submitRequestBtn = $('<a>').attr('href', '#').text('Report Issue');
+  cardBody.append(submitRequestBtn);
+
+  card.append(cardImageContainer, cardBody);
+
+  return card;
+}
+
+export { buildServiceCategoryItem, buildItAssetServiceItem };
