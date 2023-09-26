aa98bdbdd7bd7f52855c87f48e7617ae5c56edfb
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-26 19:03:50 +0500

    service item used

--- v1/service_catalog_builder.js
+++ v1/service_catalog_builder.js
@@ -226,7 +226,7 @@ function buildDetailPage(serviceCategoryItem, categoryContainerId) {
 
 function bindEventListener(serviceCategoryItem) {
   debugger;
-  $('body').on('click', '#service_item_detail_page_btn' + serviceCategoryItem.id + serviceCategory.name.toLowerCase(), function(e) {
+  $('body').on('click', '#service_item_detail_page_btn' + serviceCategoryItem.id + serviceCategoryItem.name.toLowerCase(), function(e) {
     e.preventDefault();
 
     const id   = $(this).data('id');
