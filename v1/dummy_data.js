ab281dfffc75b6f4dc81faae2682a1cdae54de2c
Mehboob Ali <mehboob.ali@7vals.com>
2023-09-27 11:40:12 +0500

    demo data updated

--- v1/dummy_data.js
+++ v1/dummy_data.js
@@ -48,6 +48,17 @@ const zendeskTicketFormData = {
       'custom_field_value':   'Request New Software',
       'ticket_form_subject':  'Request New Software - '
     }
+  },
+  'request_laptops': {
+    queryParams: {
+      'ticket_form_id':       '14009001995154',
+      'service_category':     'request_laptops'
+    },
+    ticketFormData: {
+      'custom_field_id':      '14008974403346',
+      'custom_field_value':   'Request New Laptop',
+      'ticket_form_subject':  'Request Laptops - '
+    }
   }
 };
 
@@ -57,20 +68,20 @@ const serviceCategoriesItems = {
     description: 'List of everything within your custody. Easily report issues with your assigned devices or software.',
     serviceItems: [
       { 
-        id:    '134',
-        name:  'Dell Inspiron 15 311',
-        type:  'assigned_it_asset',  
-        img_src: 'adasd',
+        id:       '134',
+        name:     'Dell Inspiron 15 311',
+        type:     'assigned_it_asset',  
+        img_src:  'https://via.placeholder.com/100x100',
         display_fields: [ 
           { label: 'Serial #',    value: 'DSW42F4F' },
           { label: 'Assigned On', value: '7 June 2021' }
         ]
       },
       {
-        id:    '135',
-        name:  'Jabra Evolve Headset',
-        type:  'assigned_it_asset',   
-        img_src: 'asdasd',
+        id:       '135',
+        name:     'Jabra Evolve Headset',
+        type:     'assigned_it_asset',   
+        img_src:  'https://via.placeholder.com/100x100',
         display_fields: [
           { label: 'Serial #',    value: 'DSW42F4F' },
           { label: 'Assigned On', value: '7 June 2021' }
@@ -80,7 +91,7 @@ const serviceCategoriesItems = {
         id:      '136',
         name:    'Zoom',
         type:    'assigned_software_entitlement',
-        img_src: 'asdasd',
+        img_src: 'https://via.placeholder.com/100x100',
         display_fields: [
           { label: 'Seats Given', value: '1' },
           { label: 'Assigned On', value: '7 June 2021' }
@@ -95,8 +106,7 @@ const serviceCategoriesItems = {
       {
         id:      '1',
         name:    'Request New Software',
-        type:    'software_request',
-        img_src: 'asdsad',
+        img_src: 'https://via.placeholder.com/100x100',
         description: "Can't find the software you're looking for? Request a new one!",
         detail_page_fields: [
           { label: 'Details', value: "Can't find the software you're looking for? Request a new one!" }
@@ -105,13 +115,74 @@ const serviceCategoriesItems = {
       {
         id:      '2',
         name:    'Slack',
-        type:    'software_request',
         price:   '10.99',
-        img_src: 'sdasd',
+        img_src: 'https://via.placeholder.com/100x100',
         description: "SlackConnect Plus is the ultimate productivity-enhancing app for Slack, designed to supercharge your team's collaboration and communication experience. Elevate your Slack workspace to a whole new level with a range of powerful features that streamline workflows, boost efficiency, and facilitate seamless collaboration like never before.",
         detail_page_fields: [
           { label: 'Details',      value: "SlackConnect Plus is the ultimate productivity-enhancing app for Slack, designed to supercharge your team's collaboration and communication experience. Elevate your Slack workspace to a whole new level with a range of powerful features that streamline workflows, boost efficiency, and facilitate seamless collaboration like never before." },
-          { label: 'Key features', value: "test" }
+          { label: 'Key features', value: "WIP" }
+        ]
+      },
+      {
+        id:       '3',
+        name:     'Zoom',
+        price:    '10.99',
+        img_src:  'https://via.placeholder.com/100x100',
+        description: 'Zoom is a video conferencing tool allowing you to meet online with other',
+        detail_page_fields: [
+          { label: 'Details',       value: 'Zoom is a video conferencing tool allowing you to meet online with other' },
+          { label: 'Key features',  value: 'WIP' }
+        ]
+      },
+      {
+        id:       '4',
+        name:     'Jira Software',
+        price:    '10.99',
+        img_src:  'https://via.placeholder.com/100x100',
+        description: 'Jira is a project management tool used by our Engineering teams',
+        detail_page_fields: [
+          { label: 'Details',       value: 'Jira is a project management tool used by our Engineering teams' },
+          { label: 'Key features',  value: 'WIP' }
+        ]
+      },
+      {
+        id:       '5',
+        name:     'SAP',
+        price:    '10.99',
+        img_src:  'https://via.placeholder.com/100x100',
+        description: 'SAP is an ERP tool currently being used to manage financial and accounting',
+        detail_page_fields: [
+          { label: 'Details',       value: 'SAP is an ERP tool currently being used to manage financial and accounting' },
+          { label: 'Key features',  value: 'WIP' }
+        ]
+      },
+      {
+        id:       '6',
+        name:     'Google Workspace',
+        price:    '10.99',
+        img_src:  'https://via.placeholder.com/100x100',
+        description: 'Google Workspace allows you access to gmail, google calendar, docs, sheets, slides',
+        detail_page_fields: [
+          { label: 'Details',       value: 'Google Workspace allows you access to gmail, google calendar, docs, sheets, slides' },
+          { label: 'Key features',  value: 'WIP' }
+        ]
+      }
+    ]
+  },
+  'request_laptops': {
+    label:       'Request Laptops',
+    description: 'Browse and request available laptops for your needs or request a new one!',
+    serviceItems: [
+      {
+        id:      '1',
+        name:    'Standard Laptop',
+        img_src: 'https://via.placeholder.com/100x100',
+        description: "Request a standard laptop for your daily work tasks and productivity needs",
+        detail_page_fields: [
+          { label: 'Details',         value: "Request a standard laptop for your daily work tasks and productivity needs" },
+          { label: 'Specifications',  value: "Processor: Intel Core i5, RAM: 8GB, Storage: 256GB SSD, Display: '14-inch FULL HD" },
+          { label: '',                value: "To request a Standard laptop, simply click the 'Request Service' button" },
+          { label: '',                value: "Please note that access to laptops may be subject to approval based on your role and team requirements" }
         ]
       }
     ]
