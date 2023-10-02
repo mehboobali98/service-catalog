0412aa39348052efeb551af8dcc10248f8757127
Mehboob Ali <mehboob.ali@7vals.com>
2023-10-02 16:43:40 +0500

    link used for svg

--- v1/service_catalog_builder.js
+++ v1/service_catalog_builder.js
@@ -179,7 +179,7 @@ class ServiceCatalogBuilder {
     const loadingIconContainer    = $('<div>').attr('id', 'loading_icon_container')
                                               .addClass('col-10');
     const loadingIconFlex         = $('<div>').addClass('d-flex flex-column align-items-center');
-    const loadingIcon             = $('<img>').attr({ 'src': './svg/loading_icon.svg',
+    const loadingIcon             = $('<img>').attr({ 'src': 'https://s2.svgbox.net/loaders.svg?ic=puff',
                                                       'alt': 'Loading...'
                                                     });
     loadingIconFlex.append(loadingIcon);
