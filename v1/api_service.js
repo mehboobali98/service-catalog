f059b6c149f08d7efb4a9faeabcad596d42f1ed1
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-06 16:02:36 +0500

    api service added

---
+++ v1/api_service.js
@@ -0,0 +1,44 @@
+class ApiService {
+  constructor(ezoSubdomain) {
+    this.ezoSubdomain = ezoSubdomain;
+  }
+
+  fetchServiceCategoriesAndItems() {
+    $.getJSON('/hc/api/v2/integration/token')
+      .then(data => data.token)
+      .then(token => {
+        if (token) {
+          const options = { method: 'GET', headers: { 'Authorization': 'Bearer ' + token, 'ngrok-skip-browser-warning': true } };
+          const endPoint = 'visible_service_categories_and_items';
+          const url = 'https://' + this.ezoSubdomain + '/webhooks/zendesk/' + endPoint + '.json';
+
+          fetch(url, options)
+            .then(response => {
+
+              if (response.status === 400) {
+                throw new Error('Bad Request: There was an issue with the request.');
+              } else if (response.status === 404) {
+                throw new Error('Not Found: User account was not found.');
+              }
+
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+
+              debugger;
+              return response.json();
+            })
+            .then(data => {
+              debugger;
+              return JSON.parse(data);
+            })
+            .catch(error => {
+              debugger;
+              alert('An error occurred while fetching service categories and items: ' + error.message);
+            });
+        }
+      });
+  }
+}
+
+export { ApiService };
