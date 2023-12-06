9c0b3efcab439ae2bd4c06e1000ccad7b2c43c01
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-06 17:05:48 +0500

    returned

--- v1/api_service.js
+++ v1/api_service.js
@@ -4,38 +4,38 @@ class ApiService {
   }
 
   fetchServiceCategoriesAndItems() {
-    $.getJSON('/hc/api/v2/integration/token')
-      .then(data => data.token)
-      .then(token => {
-        if (token) {
-          const options = { method: 'GET', headers: { 'Authorization': 'Bearer ' + token, 'ngrok-skip-browser-warning': true } };
-          const endPoint = 'visible_service_categories_and_items';
-          const url = 'https://' + this.ezoSubdomain + '/webhooks/zendesk/' + endPoint + '.json';
-          debugger;
+    return $.getJSON('/hc/api/v2/integration/token')
+            .then(data => data.token)
+            .then(token => {
+              if (token) {
+                const options = { method: 'GET', headers: { 'Authorization': 'Bearer ' + token, 'ngrok-skip-browser-warning': true } };
+                const endPoint = 'visible_service_categories_and_items';
+                const url = 'https://' + this.ezoSubdomain + '/webhooks/zendesk/' + endPoint + '.json';
+                debugger;
 
-          fetch(url, options)
-            .then(response => {
+                fetch(url, options)
+                  .then(response => {
 
-              debugger;
-              if (response.status === 400) {
-                throw new Error('Bad Request: There was an issue with the request.');
-              } else if (response.status === 404) {
-                throw new Error('Not Found: User account was not found.');
+                    debugger;
+                    if (response.status === 400) {
+                      throw new Error('Bad Request: There was an issue with the request.');
+                    } else if (response.status === 404) {
+                      throw new Error('Not Found: User account was not found.');
+                    }
+
+                    if (!response.ok) {
+                      throw new Error('Network response was not ok');
+                    }
+
+                    debugger;
+                    return response.json();
+                  })
+                  .catch(error => {
+                    debugger;
+                    alert('An error occurred while fetching service categories and items: ' + error.message);
+                  });
               }
-
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-
-              debugger;
-              return response.json();
-            })
-            .catch(error => {
-              debugger;
-              alert('An error occurred while fetching service categories and items: ' + error.message);
             });
-        }
-      });
   }
 }
 
