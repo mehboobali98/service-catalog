49884b18c8dee9dc34cb9929298d955d8c941995
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-08 23:46:00 +0500

    console error used

--- v1/api_service.js
+++ v1/api_service.js
@@ -30,7 +30,7 @@ class ApiService {
               callback(data);
             })
             .catch(error => {
-              console.log('An error occurred while fetching service categories and items: ' + error.message);
+              console.error('An error occurred while fetching service categories and items: ' + error.message);
             });
         }
     });
