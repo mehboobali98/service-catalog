b1155f6ad0e5e04faed94f494fe2130cb7eea4d7
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-14 13:52:40 +0500

    issue fixed

--- v1/constant.js
+++ v1/constant.js
@@ -5,13 +5,13 @@ const SERVICE_ITEM_PLACEHOLDER_IMAGE_MAPPING = {
 };
 
 const HELP_CENTER_TO_EZ_CLASS_MAPPING = {
-  'text_font:'          'ez_text_color',
+  'text_font':          'ez_text_color',
   'text_color':         'ez_text_color',
   'link_color':         'ez_link_color',
   'brand_color':        'ez_brand_color',
   'heading_font':       'ez_heading_font',
   'background_color':   'ez_background_color',
   'brand_text_color':   'ez_brand_text_color'
-}
+};
 
 export { SERVICE_ITEM_PLACEHOLDER_IMAGE_MAPPING, HELP_CENTER_TO_EZ_CLASS_MAPPING };
