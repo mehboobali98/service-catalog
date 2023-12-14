63c08f69b732e8521c0e5a4cca56913f361780fc
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-14 12:45:26 +0500

    missing colon added

--- v1/constant.js
+++ v1/constant.js
@@ -1,5 +1,5 @@
 const SERVICE_ITEM_PLACEHOLDER_IMAGE_MAPPING = {
-  'service_item'                 'service_item_placeholder',
+  'service_item':                'service_item_placeholder',
   'assigned_asset':              'asset_placeholder',
   'assigned_software_license':   'software_license_placeholder'
 };
