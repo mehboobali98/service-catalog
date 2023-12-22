7ef4ca4bfaebdcc995de43a45ce5a35c9c18455d
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-22 16:09:50 +0500

    arrow function used

--- v1/service_catalog_item_builder.js
+++ v1/service_catalog_item_builder.js
@@ -73,7 +73,7 @@ class ServiceCatalogItemBuilder {
     }
   }
 
-  buildItAssetServiceItem(serviceCategory, serviceCategoryItem) {
+  buildItAssetServiceItem = (serviceCategory, serviceCategoryItem) => {
     const card        = $('<div>').addClass('row service-item-card');
     const queryParams = {};
 
