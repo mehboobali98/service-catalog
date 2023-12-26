9a4a4e32419d7f858ca1b2408af671e92a4c6d3c
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-26 16:44:40 +0500

    text updated

--- v1/view_helper.js
+++ v1/view_helper.js
@@ -3,7 +3,7 @@ function noResultsFound() {
                                      .addClass('d-flex flex-column align-items-center no-results-container');
   const noResultsImage  = $('<img>').attr('src', 'https://raw.githubusercontent.com/mehboobali98/service-catalog/service_catalog/v1/assets/svg/no_results_found.svg')
                                     .addClass('no-results-image');
-  const noResultsLabel  = $('<p>').text('No Search Results Found')
+  const noResultsLabel  = $('<p>').text('No Result Found')
                                   .addClass('no-results-message');
   noResultsContainer.append(noResultsImage, noResultsLabel);
   return noResultsContainer;
