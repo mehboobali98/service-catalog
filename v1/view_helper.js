4e66f7e4a3cc18e4a84d7c1fd9c0ca1ecc5f929e
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-26 16:25:56 +0500

    fix

--- v1/view_helper.js
+++ v1/view_helper.js
@@ -1,14 +1,15 @@
 function noResultsFound() {
-  const container       = $('div').attr('id', 'no_results_container')
-                                  .addClass('d-flex flex-column align-items-center no-results-container');
+  const noResultsContainer = $('<div>').attr('id', 'no_results_container')
+                                     .addClass('d-flex flex-column align-items-center no-results-container');
   const noResultsImage  = $('<img>').attr('src', 'https://raw.githubusercontent.com/mehboobali98/service-catalog/service_catalog/v1/assets/svg/no_results_found.svg')
                                     .addClass('no-results-image');
   const noResultsLabel  = $('<p>').text('No Search Results Found')
                                   .addClass('no-results-message');
-  container.append(noResultsImage, noResultsLabel);
+  debugger;
+  noResultsContainer.append(noResultsImage, noResultsLabel);
 
   debugger;
-  return container;
+  return noResultsContainer;
 }
 
 export { noResultsFound };
