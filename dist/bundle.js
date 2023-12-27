ee187ff86c379181f92a3502b22d0f3f451d46b8
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-27 14:53:14 +0500

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -1179,7 +1179,7 @@
     filesToLoad() {
       return [
                 { type: 'link',   url: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' },
-                { type: 'link',   url: 'https://mehboobali98.github.io/service-catalog/code_restructure/assets/stylesheets/service_catalog.css' },
+                { type: 'link',   url: 'https://mehboobali98.github.io/service-catalog/assets/stylesheets/service_catalog.css' },
                 { type: 'script', url: 'https://code.jquery.com/jquery-3.6.0.min.js' }
              ];
     }
