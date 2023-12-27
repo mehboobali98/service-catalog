cb3103c2c83411299c49ce28041f4ad59c5ff08c
Mehboob Ali <mehboob.ali@7vals.com>
2023-12-27 13:10:28 +0500

    add

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     ],
+  },
 };
