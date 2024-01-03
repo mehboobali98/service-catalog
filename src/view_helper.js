f45237c8fefad9b15a3abbc971529699c078aa35
Mehboob Ali <mehboob.ali@7vals.com>
2024-01-04 00:57:05 +0500

    issue fixed

--- src/view_helper.js
+++ src/view_helper.js
@@ -4,7 +4,7 @@ import { STAGING_CDN_URL,
 function noResultsFound() {
   const noResultsContainer = $('<div>').attr('id', 'no_results_container')
                                        .addClass('d-flex flex-column align-items-center no-results-container');
-  const noResultsImage  = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/assets/images/svg/no_results_found.svg`);
+  const noResultsImage  = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/assets/images/svg/no_results_found.svg`)
                                     .addClass('no-results-image');
   const noResultsLabel  = $('<p>').text('No Result Found')
                                   .addClass('no-results-message');
