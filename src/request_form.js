b5bc16bfce7fa371655c6090d36dbc5c58028625
Mehboob Ali <mehboob.ali@7vals.com>
2024-01-12 17:06:51 +0500

    logic updated

--- src/request_form.js
+++ src/request_form.js
@@ -16,7 +16,6 @@ class RequestForm {
       const ezoFieldData            = data.request.custom_fields.find(function (customField) { return customField.id == self.ezoFieldId });
       const ezoServiceItemFieldData = data.request.custom_fields.find(function (customField) { return customField.id == self.ezoServiceItemFieldId });
 
-      debugger;
       const ezoFieldDataPresent            = self.fieldDataPresent(ezoFieldData);
       const ezoServiceItemFieldDataPresent = self.fieldDataPresent(ezoServiceItemFieldData); 
 
@@ -29,7 +28,7 @@ class RequestForm {
         if (token) {
           options.headers['Authorization'] = 'Bearer ' + token;
 
-          if (ezoServiceItemFieldDataPresent) { self.processEzoServiceItemField(requestId); }
+          if (ezoServiceItemFieldDataPresent) { self.linkResources(requestId, { serviceItemFieldId: self.serviceItemFieldId }); }
 
           const parsedEzoFieldValue = JSON.parse(ezoFieldData.value);
           const assetSequenceNums   = parsedEzoFieldValue.assets.map(asset => Object.keys(asset)[0]);
@@ -70,10 +69,6 @@ class RequestForm {
     return $.getJSON('/hc/api/v2/integration/token').then(data => {
       return callback(data.token);
     })
-  }
-
-  processEzoServiceItemField(requestId) {
-    return linkResources(requestId, { serviceItemFieldId: this.serviceItemFieldId });
   }
 
   linkResources(requestId, options) {
