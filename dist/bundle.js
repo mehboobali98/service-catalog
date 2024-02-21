295c5822eb64d06bbbe95686adde04050de0de5e
Mehboob Ali <mehboob.ali@7vals.com>
2024-02-21 15:38:08 +0500

    new builds added

--- dist/bundle.js
+++ dist/bundle.js
@@ -487,6 +487,26 @@
     return serviceCatalogDisabledContainer;
   }
 
+  function serviceCatalogEmpty(ezoSubdomain) {
+    const serviceCatalogEmptyContainer    = $('<div>').addClass('d-flex flex-column align-items-center');
+    const serviceCategoryImage            = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/assets/images/svg/service_category.svg`)
+                                                      .addClass('no-access-image');
+
+    const nextStepsMessage                = $('<p>').text('Please create and enable service categories in the builder to start using Service Catalog.')
+                                                    .addClass('next-steps-message');
+
+    // button
+    const buttonsContainer                = $('<div>').addClass('d-flex mt-3 gap-3 justify-content-end');
+    const serviceCatalogBuilderUrl        = `https://${ezoSubdomain}.assetsonar.com/service_catalog/builder`;
+    const serviceCatalogBtn               = $('<a>').attr('href', serviceCatalogBuilderUrl)
+                                                    .text('Go to Service Catalog Builder')
+                                                    .addClass('btn btn-outline-primary go-back-btn');
+    buttonsContainer.append(serviceCatalogBtn);
+
+    serviceCatalogEmptyContainer.append(serviceCategoryImage, nextStepsMessage, buttonsContainer);
+    return serviceCatalogEmptyContainer;
+  }
+
   function noResultsFound() {
     const noResultsContainer = $('<div>').attr('id', 'no_results_container')
                                          .addClass('d-flex flex-column align-items-center no-results-container');
