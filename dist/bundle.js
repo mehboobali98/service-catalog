c1ab2cc0aa5784e59bdbdb472d07cfe7ff6ae105
Mehboob Ali <mehboob.ali@7vals.com>
2024-05-02 11:45:39 +0500

    testing local changes

--- dist/bundle.js
+++ dist/bundle.js
@@ -32,7 +32,8 @@
 
   // Retrieve translations JSON object for the given locale over the network
   function fetchTranslationsFor(newLocale) {
-    return $.getJSON(`${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/i18n/${newLocale}.json`);
+    return $.getJSON(`https://mehboobali98.github.io/service-catalog/dist/public/i18n/${newLocale}.json`);
+    //return $.getJSON(`${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/i18n/${newLocale}.json`);
   }
 
   // Replace the inner text of each element that has a
@@ -552,7 +553,7 @@
 
   function serviceCatalogDisabled(ezoSubdomain) {
     const serviceCatalogDisabledContainer = $('<div>').addClass('d-flex flex-column align-items-center service-catalog-disabled-container');
-    const noAccessImage                   = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/images/svg/no_access_image.svg`)
+    const noAccessImage                   = $('<img>').attr('src', `https://mehboobali98.github.io/service-catalog/dist/public/images/svg/no_access_image.svg`)
                                                       .addClass('no-access-image');
 
     const nextStepsMessage                = $('<p>').attr('data-i18n', 'enable-service-catalog')
@@ -574,7 +575,7 @@
 
   function serviceCatalogEmpty(ezoSubdomain) {
     const serviceCatalogEmptyContainer    = $('<div>').addClass('d-flex flex-column align-items-center service-catalog-empty-container');
-    const serviceCategoryImage            = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/images/svg/service_category.svg`)
+    const serviceCategoryImage            = $('<img>').attr('src', `https://mehboobali98.github.io/service-catalog/dist/public/images/svg/service_category.svg`)
                                                       .addClass('no-access-image');
 
     const nextStepsMessage                = $('<p>').attr('data-i18n', 'create-and-enable-service-categories')
@@ -598,7 +599,7 @@
     const noResultsContainer  = $('<div>').attr('id', 'no_results_container')
                                           .addClass('d-flex flex-column align-items-center no-results-container');
 
-    const noResultsImage      = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/images/svg/no_results_found.svg`)
+    const noResultsImage      = $('<img>').attr('src', `https://mehboobali98.github.io/service-catalog/dist/public/images/svg/no_results_found.svg`)
                                           .addClass('no-results-image');
 
     const noResultsLabel      = $('<p>').attr('data-i18n', 'no-results-found')
@@ -613,7 +614,7 @@
     const noResultsContainer  = $('<div>').attr('id', 'no_service_items_found_container')
                                          .addClass('d-flex flex-column align-items-center no-results-container');
 
-    const noResultsImage      = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/images/svg/service_asset.svg`)
+    const noResultsImage      = $('<img>').attr('src', `https://mehboobali98.github.io/service-catalog/dist/public/images/svg/service_asset.svg`)
                                           .addClass('no-results-image');
 
     const noResultsLabel      = $('<p>').attr('data-i18n', notFoundMessageKey)
@@ -1381,7 +1382,7 @@
                                                 .addClass('no-access-page-section');
 
       const noAccessPageContainer = $('<div>').addClass('d-flex flex-column align-items-center');
-      const noAccessImage         = $('<img>').attr('src', `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/images/svg/no_access_image.svg`)
+      const noAccessImage         = $('<img>').attr('src', `https://mehboobali98.github.io/service-catalog/dist/public/images/svg/no_access_image.svg`)
 
                                               .addClass('no-access-image');
 
@@ -1453,7 +1454,8 @@
     filesToLoad() {
       return [
                 { type: 'link',   url: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' },
-                { type: 'link',   url: `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/stylesheets/service_catalog.css?${this.timeStamp}`},
+                // { type: 'link',   url: `${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/stylesheets/service_catalog.css?${this.timeStamp}`},
+                { type: 'link',   url: `https://mehboobali98.github.io/service-catalog/dist/public/stylesheets/service_catalog.css?${this.timeStamp}`},
                 { type: 'script', url: 'https://code.jquery.com/jquery-3.6.0.min.js' }
              ];
     }
