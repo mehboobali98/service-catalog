a45eda0e51b6ed6cf0013bf8523296c8719dd403
Mehboob Ali <mehboob.ali@7vals.com>
2024-05-06 18:20:06 +0500

    path updated for i18n and new build

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,7 @@ function setLocale(newLocale, shouldTranslatePage) {
 
 // Retrieve translations JSON object for the given locale over the network
 function fetchTranslationsFor(newLocale) {
-  return $.getJSON(`${PRODUCTION_CDN_URL}/shared/service_catalog/dist/public/i18n/${newLocale}.json`);
+  return $.getJSON(`https://mehboobali98.github.io/service-catalog/dist/public/${newLocale}.json`);
 }
 
 // Replace the inner text of each element that has a
