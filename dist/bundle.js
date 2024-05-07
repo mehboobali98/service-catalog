30046e172aa9b05e8eef3f68e1a1b59e31709649
Mehboob Ali <mehboob.ali@7vals.com>
2024-05-07 18:57:59 +0500

    remove debugger

--- dist/bundle.js
+++ dist/bundle.js
@@ -1187,7 +1187,6 @@
       const cardImageContainer    = $('<div>').addClass('col-4');
       const cardImageFlex         = $('<div>').addClass('d-flex flex-column h-100 service-item-card-image-container');
       const placeholderPath       = placeholderImagePath(serviceCategoryItem);
-      debugger;
       const cardImage             = $('<img>').attr('src', serviceCategoryItem.display_picture_url)
                                               .attr('alt', 'IT Asset')
                                               .addClass('w-100')
