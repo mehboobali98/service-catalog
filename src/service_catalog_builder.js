50d913341c384e2e335c2f30b3f6d51e4816caac
Mehboob Ali <mehboob.ali@7vals.com>
2024-06-05 03:40:32 +0500

    more debuggers

--- src/service_catalog_builder.js
+++ src/service_catalog_builder.js
@@ -95,6 +95,7 @@ class ServiceCatalogBuilder {
 
     searchAndNavContainer.append(navbarContainer);
     const serviceItemsContainer   = this.serviceCatalogItemBuilder.build(this.data);
+    debugger;
     const searchResultsContainer  = $('<div>').attr('id', 'service_catalog_item_search_results_container')
                                               .addClass('col-10 collapse service-catalog-search-results-container');
     serviceCatalogContainer.append(searchAndNavContainer, serviceItemsContainer, searchResultsContainer);
@@ -102,6 +103,7 @@ class ServiceCatalogBuilder {
 
     $('main').append(newSection);
     this.serviceCatalogItemDetailBuilder.build(this.data);
+    debugger;
     this.bindEventListeners();
     this.addTooltipsForTruncatedText();
   }
@@ -227,7 +229,13 @@ class ServiceCatalogBuilder {
       return false;
     });
 
-    $('body').on('click', '.js-service-item-card', function(e) {
+    $('.js-service-item-request-btn').click(function(e) {
+      debugger;
+    });
+
+
+
+    $('body').on('click', '.js-service-item-request-btn', function(e) {
       debugger;
     });
   }
