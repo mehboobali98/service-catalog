2607a9174317e1540e3584decfa3162de16075e7
Mehboob Ali <mehboob.ali@7vals.com>
2024-07-03 12:22:23 +0500

    build

--- dist/bundle.js
+++ dist/bundle.js
@@ -632,6 +632,7 @@
               const assetSequenceNums   = parsedEzoFieldValue.assets.map(asset => Object.keys(asset)[0]);
               const assetNames          = parsedEzoFieldValue.assets.map(asset => Object.values(asset)[0]);
 
+              debugger;
               if (!assetSequenceNums || assetSequenceNums.length == 0 || !ezoServiceItemFieldData) { return true; }
 
               debugger;
