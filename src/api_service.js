aa30bbc988597bab85fe62e9e5326b781735a15b
Mehboob Ali <mehboob.ali@7vals.com>
2025-04-30 06:43:47 +0500

    debugger added

--- src/api_service.js
+++ src/api_service.js
@@ -125,6 +125,21 @@ class ApiService {
                             display_picture_url:              record.custom_object_fields.display_picture_url || '',
                             service_category_title_with_id:   categoryKey
                           });
+                        } else if (resourceType === 'StockAsset') {
+                          debugger;
+                          restructuredData[categoryKey].service_items.push({
+                            id: record.custom_object_fields.asset_id,
+                            name: record.custom_object_fields.asset_name || record.name, 
+                            display_fields: {
+                              'AIN':       record.custom_object_fields.identifier,
+                              'Asset #':   record.custom_object_fields.sequence_num,
+                              'Location':  record.custom_object_fields.location
+                            },
+                            sequence_num:                     record.custom_object_fields.sequence_num,
+                            zendesk_form_id:                  record.custom_object_fields.zd_form_id || null,
+                            display_picture_url:              record.custom_object_fields.display_picture_url || '',
+                            service_category_title_with_id:   categoryKey
+                          });
                         } else if (resourceType === 'SoftwareLicense') {
                           restructuredData[categoryKey].service_items.push({
                             id: record.custom_object_fields.asset_id,
